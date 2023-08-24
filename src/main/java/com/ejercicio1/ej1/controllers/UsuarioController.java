0541347a473666597a69585016d04959cb9e1818
Agustín <agustin.fidelio@gmail.com>
2023-08-24 16:30:47 -0300

    user controller

--- src/main/java/com/ejercicio1/ej1/controllers/UsuarioController.java
+++ src/main/java/com/ejercicio1/ej1/controllers/UsuarioController.java
@@ -1,5 +1,6 @@
 package com.ejercicio1.ej1.controllers;
 
+import com.ejercicio1.ej1.models.Usuario;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class UsuarioController {
 
     @RequestMapping(value = "usuarios")
-    public String getUsuarios() {
-        return "asdf";
+    public Usuario getUsuarios() {
+        Usuario usuario = new Usuario();
+        usuario.setNombre("juan");
+        usuario.setApellido("Gonzales");
+        usuario.setEmail("juan@gmail.com");
+        usuario.setTelefono("451122321");
+        return usuario;
     }
 
 }
