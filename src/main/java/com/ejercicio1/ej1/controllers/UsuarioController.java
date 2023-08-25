97f0a8be8363567edfb321725c12d8a3fddcd881
Agustín <agustin.fidelio@gmail.com>
2023-08-25 14:14:21 -0300

    delete user

--- src/main/java/com/ejercicio1/ej1/controllers/UsuarioController.java
+++ src/main/java/com/ejercicio1/ej1/controllers/UsuarioController.java
@@ -5,6 +5,7 @@ import com.ejercicio1.ej1.models.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,10 +17,23 @@ public class UsuarioController {
     @Autowired
     private UsuarioDao usuarioDao;
 
-    @RequestMapping(value = "usuarios")
+    @RequestMapping(value = "api/usuarios", method = RequestMethod.GET)
     public List<Usuario> getUsuarios() {
         return usuarioDao.getUsuarios();
     }
+
+    @RequestMapping(value = "api/usuarios/{id}", method = RequestMethod.DELETE)
+    public void eliminar(@PathVariable Long id) {
+        usuarioDao.eliminar(id);
+    }
+
+
+
+
+  /* @RequestMapping(value = "api/usuario", method = RequestMethod.GET)
+    public List<Usuario> getUsuario() {
+        return usuarioDao.getUsuario();
+    } */
 
 }
 
